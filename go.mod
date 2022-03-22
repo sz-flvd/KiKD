129721a4b1bcee83eecb68801c089667b760f38c
sz-flvd <skdprzybysz@gmail.com>
2022-03-22 23:42:53 +0100

    Update project structure

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module KiKD
 
 go 1.18
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"KiKD/lab2"
+	"fmt"
+)
+
+func arithmeticCoding() {
+	lab2.Encode()
+	lab2.Decode()
+}
+
+func main() {
+	fmt.Println("yoyo")
+}
